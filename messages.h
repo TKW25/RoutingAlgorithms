b1f3dcc944c2589a74f9e538250363d9bcc8c751
TKW <TKW25@users.noreply.github.com>
2017-12-07 21:38:28 -0500

    Update messages.h

--- messages.h
+++ messages.h
@@ -7,6 +7,18 @@ class Node;
 #include "table.h"
 #include "linkstate.h"
 class LinkState;
+
+struct LinkCosts{
+    LinkCosts(){ destCost.clear(); src = 0; }
+    LinkCosts(unsigned s, double d, unsigned ss){
+        destCost.clear();
+        destCost.insert(pair<unsigned, double>(s, d));
+        src = ss;
+    }
+    map<unsigned, double> destCost;
+    unsigned src;
+};
+
 struct RoutingMessage {
     RoutingMessage();
     RoutingMessage(const RoutingMessage &rhs);
